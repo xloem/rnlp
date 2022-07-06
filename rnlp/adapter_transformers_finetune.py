84448d626b34f349cabb74b3b5953e78028f3e4e
John Doe <johndoe@example.com>
2022-07-06 16:06:08 -0400

    -

--- rnlp/adapter_transformers_finetune.py
+++ rnlp/adapter_transformers_finetune.py
@@ -1,6 +1,6 @@
 import transformers
 
-def go(model_name, task_name, adapter_config, labels=None, config_name = None, tokenizer_name=None, revision=None, adapter_non_linearity = None, adapter_reduction_factor = None, load_adapter=None, extra_adapters={}):
+def init(model_name, task_name, adapter_config, labels=None, config_name = None, tokenizer_name=None, revision=None, adapter_non_linearity = None, adapter_reduction_factor = None, load_adapter=None, extra_adapters={}):
     config = transformers.AutoConfig.from_pretrained(config_name if config_name else model_name, num_labels=len(labels) if labels else None, revision=revision, finetuning_task=task_name)
     tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_name if tokenizer_name else model_name, revision=revision, use_fast=True)
 
@@ -28,6 +28,18 @@ def go(model_name, task_name, adapter_config, labels=None, config_name = None, t
         model.train_adapter([task_name])
 
         model.set_active_adapters(ac.Stack([*extra_adapters.keys(), task_name])
+        return model
     else:
         if load_adapter or len(extra_adapters):
             raise ValueError('this code was copied from code that does not handle loading adapters when not training a task')
+    
+
+model = init()
+optim = torch.optim.Adam(model.parameters(), lr=1e-5, betas=(0.9,0.999), eps=4e-9)
+
+while True:
+    loss = model(input_ids, labels)
+    model.zero_grad()
+    loss.backward()
+    optim.step()
+
